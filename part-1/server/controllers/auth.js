76ffeff4154980fa0115dc444374678ba54b0c52
Laman Ismayilova <laman.mamedova98@gmail.com>
2023-08-03 13:15:29 -0700

    crypto-security

---
+++ part-1/server/controllers/auth.js
@@ -0,0 +1,31 @@
+const bcrypt=require("bcryptjs")
+const users = []
+
+module.exports = {
+    login: (req, res) => {
+      let {username,password}=req.body
+      let checkUser = users.filter((user)=>user.username===username)
+      if(checkUser.length!==0){
+          let validPassword=bcrypt.compareSync(password,checkUser[0].hashedPass)
+          if(validPassword){
+              return res.status(200).send("Successfully logged in ")
+          }else{
+              return res.status(401).send("Password incorrect")
+          }
+      }else{
+          return res.status(401).send("No user found by that name.")
+      }
+    },
+    register: (req, res) => {
+      let {username,password}=req.body
+      let salt=bcrypt.genSaltSync(10)
+      let hashedPass=bcrypt.hashSync(password,salt)
+         console.log(hashedPass)
+         let newUser = {
+          username:username,
+          hashedPass:hashedPass
+         }
+        users.push(newUser)
+        res.status(200).send(newUser)
+    }
+}
